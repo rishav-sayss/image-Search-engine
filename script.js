68e4171f59a1f6ba7e809d4c3e97756e3f432b2e
Rishabh Shrivas <rishavshrivas24@gmail.com>
2025-11-21 18:28:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+//  let UNSPLASH_KEY = "MfjX02hGSbUjetebdd6raAbRxt1_m5w59vtcjabyVtI";
+let input = document.querySelector("#input");
+let form = document.querySelector("#form");
+let showresult = document.querySelector(".showresult");
+let moreimg = document.querySelector(".moreimg");
+let keyword = "";
+let page = 1;
+async function searchimg() {
+    keyword = input.value;
+    let url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${UNSPLASH_KEY}&per_page=12`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let result = data.results.slice(0,9);
+    if(page===1){
+        showresult.innerHTML="";
+    }
+    result.forEach((val,index) => {
+        let img = document.createElement("img");
+        img.classList.add(index);
+        img.src = val.urls.small;
+        let imagelink = document.createElement("a");
+        imagelink.href = val.links.html;
+        imagelink.target = "_blank"
+        imagelink.appendChild(img);
+        showresult.appendChild(imagelink);
+    });
+    moreimg.style.display = "block"
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (input.value.trim() === "") {
+        alert("Please enter something");
+        return;
+    }
+    page = 1;
+    searchimg();
+})
+
+moreimg.addEventListener("click",()=>{
+    page++;
+    searchimg();
+})
